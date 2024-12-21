74070fa7f25885ccb65cea79b1293bd9544d4db1
kergosdyr <kergosdyr@kakao.com>
2024-12-21 17:40:00 +0900

    init(all): :tada: init repo

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    java
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.spotless)
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+allprojects {
+    group = property("app.group").toString()
+}
+
+dependencyManagement {
+}
+
+dependencies {
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.data.jpa)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    annotationProcessor(libs.spring.boot.configuration.processor)
+    testImplementation(libs.spring.boot.starter.test)
+    runtimeOnly(libs.h2)
+    runtimeOnly(libs.mysql.connector)
+}
+
+// about source and compilation
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+spotless {
+    java {
+        importOrder()
+        removeUnusedImports()
+        cleanthat()
+        palantirJavaFormat().style("GOOGLE")
+        formatAnnotations()
+    }
+}
+
+
+tasks.getByName("bootJar") {
+    enabled = true
+}
+tasks.getByName("jar") {
+    enabled = false
+}
+// test tasks
+tasks.test {
+    ignoreFailures = true
+    useJUnitPlatform()
+}
