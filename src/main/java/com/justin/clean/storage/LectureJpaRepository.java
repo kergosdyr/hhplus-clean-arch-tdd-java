338133f6d2c5f65bb4757c57f780581fb438d366
kergosdyr <kergosdyr@kakao.com>
2024-12-25 21:01:25 +0900

    feature(lecture): add pessimistic lock timeout for prevent deadlock

--- src/main/java/com/justin/clean/storage/LectureJpaRepository.java
+++ src/main/java/com/justin/clean/storage/LectureJpaRepository.java
@@ -2,6 +2,8 @@ package com.justin.clean.storage;
 
 import com.justin.clean.domain.Lecture;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -9,10 +11,14 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
 public interface LectureJpaRepository extends JpaRepository<Lecture, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_READ)
+    @QueryHints({
+        @QueryHint(name = "javax.persistence.lock.timeout", value = "5000")
+    })
     @Query("select l from Lecture l where l.id = ?1")
     Optional<Lecture> findByIdWithLock(long id);
 
